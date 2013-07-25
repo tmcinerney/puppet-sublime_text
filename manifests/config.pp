ced70e80512684f436bbb7e134858bcb03160c29
Travers McInerney <travers@mcinerney.me>
2013-07-25 11:15:43 +1000

    Updated to build 3047

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,10 @@
-# Internal: Prepare your system for Sublime Text 2 packages.
+# Internal: Prepare your system for Sublime Text 3 packages.
 #
 # Examples
 #
 #   include sublime_text::config
 class sublime_text::config {
-  $dir        = "/Users/${::luser}/Library/Application Support/Sublime Text"
+  $dir        = "/Users/${::luser}/Library/Application Support/Sublime Text 3"
   $packagedir = "${dir}/Packages"
 
   file { [$dir, $packagedir]:
